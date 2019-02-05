c73eafbdc215662c2783d9182cdefc8ec38102f2
julianedwards <julian.edwards@10gen.com>
2019-02-05 17:31:49 -0500

    MAKE-627: move certificates into the database (cert depot impl) (#59)

---
+++ cert.go
@@ -0,0 +1,289 @@
+package certdepot
+
+import (
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/square/certstrap/depot"
+	"github.com/square/certstrap/pkix"
+)
+
+type CertificateOptions struct {
+	//
+	// Options specific to Init and CertRequest.
+	//
+	// Passprhase to encrypt private-key PEM block.
+	Passphrase string
+	// Size (in bits) of RSA keypair to generate (defaults to 2048).
+	KeyBits int
+	// Sets the Organization (O) field of the certificate.
+	Organization string
+	// Sets the Country (C) field of the certificate.
+	Country string
+	// Sets the Locality (L) field of the certificate.
+	Locality string
+	// Sets the Common Name (CN) field of the certificate.
+	CommonName string
+	// Sets the Organizational Unit (OU) field of the certificate.
+	OrganizationalUnit string
+	// Sets the State/Province (ST) field of the certificate.
+	Province string
+	// IP addresses to add as subject alt name.
+	IP []string
+	// DNS entries to add as subject alt name.
+	Domain []string
+	// URI values to add as subject alt name.
+	URI []string
+	// Path to private key PEM file (if blank, will generate new keypair).
+	Key string
+
+	//
+	// Options specific to Init and Sign.
+	//
+	// How long until the certificate expires.
+	Expires time.Duration
+
+	//
+	// Options specific to Sign.
+	//
+	// Host name of the certificate to be signed.
+	Host string
+	// Name of CA to issue cert with.
+	CA string
+	// Passphrase to decrypt CA's private-key PEM block.
+	CAPassphrase string
+	// Whether generated certificate should be an intermediate.
+	Intermediate bool
+}
+
+// Init initializes a new CA.
+func (opts *CertificateOptions) Init(d depot.Depot) error {
+	if opts.CommonName == "" {
+		return errors.New("must provide Command Name for CA!")
+	}
+	formattedName := strings.Replace(opts.CommonName, " ", "_", -1)
+
+	if depot.CheckCertificate(d, formattedName) || depot.CheckPrivateKey(d, formattedName) {
+		return errors.New("CA with specified name already exists!")
+	}
+
+	key, err := opts.getOrCreatePrivateKey(formattedName)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	expiresTime := time.Now().Add(opts.Expires)
+	crt, err := pkix.CreateCertificateAuthority(
+		key,
+		opts.OrganizationalUnit,
+		expiresTime,
+		opts.Organization,
+		opts.Country,
+		opts.Province,
+		opts.Locality,
+		opts.CommonName,
+	)
+	if err != nil {
+		return errors.Wrap(err, "problem creating certificate authority")
+	}
+
+	if err = depot.PutCertificate(d, formattedName, crt); err != nil {
+		return errors.Wrap(err, "problem saving certificate authority")
+	}
+
+	if opts.Passphrase != "" {
+		if err = depot.PutEncryptedPrivateKey(d, formattedName, key, []byte(opts.Passphrase)); err != nil {
+			return errors.Wrap(err, "problem saving encrypted private key")
+		}
+	} else {
+		if err = depot.PutPrivateKey(d, formattedName, key); err != nil {
+			return errors.Wrap(err, "problem saving private key")
+		}
+	}
+
+	// create an empty CRL, this is useful for Java apps which mandate a CRL
+	crl, err := pkix.CreateCertificateRevocationList(key, crt, expiresTime)
+	if err != nil {
+		return errors.Wrap(err, "problem creating certificate revocation list")
+	}
+	if err = depot.PutCertificateRevocationList(d, formattedName, crl); err != nil {
+		return errors.Wrap(err, "problem saving certificate revocation list")
+	}
+
+	return nil
+}
+
+// CertRequest creates a new certificate (CSR).
+func (opts *CertificateOptions) CertRequest(d depot.Depot) error {
+	ips, err := pkix.ParseAndValidateIPs(strings.Join(opts.IP, ","))
+	if err != nil {
+		return errors.Wrapf(err, "problem parsing and validating IPs: %s", opts.IP)
+	}
+
+	uris, err := pkix.ParseAndValidateURIs(strings.Join(opts.URI, ","))
+	if err != nil {
+		return errors.Wrapf(err, "problem parsing and validating URIs: %s", opts.URI)
+	}
+
+	name, err := opts.getCertificateRequestName()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	formattedName, err := formatName(name)
+	if err != nil {
+		return errors.Wrap(err, "problem getting formatted name")
+	}
+
+	if depot.CheckCertificateSigningRequest(d, formattedName) || depot.CheckPrivateKey(d, formattedName) {
+		return errors.New("certificate request has existed!")
+	}
+
+	key, err := opts.getOrCreatePrivateKey(formattedName)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	csr, err := pkix.CreateCertificateSigningRequest(
+		key,
+		opts.OrganizationalUnit,
+		ips,
+		opts.Domain,
+		uris,
+		opts.Organization,
+		opts.Country,
+		opts.Province,
+		opts.Locality,
+		name,
+	)
+	if err != nil {
+		return errors.Wrap(err, "problem creating certificate request")
+	}
+
+	if err = depot.PutCertificateSigningRequest(d, formattedName, csr); err != nil {
+		return errors.Wrap(err, "problem saving certificate request")
+	}
+	if opts.Passphrase != "" {
+		if err = depot.PutEncryptedPrivateKey(d, formattedName, key, []byte(opts.Passphrase)); err != nil {
+			return errors.Wrap(err, "problem saving encrypted private key")
+		}
+	} else {
+		if err = depot.PutPrivateKey(d, formattedName, key); err != nil {
+			return errors.Wrap(err, "problem saving private key error")
+		}
+	}
+
+	return nil
+}
+
+// Sign signs a CSR with a given CA for a new certificate.
+func (opts *CertificateOptions) Sign(d depot.Depot) error {
+	if opts.Host == "" {
+		return errors.New("must provide name of host!")
+	}
+	if opts.CA == "" {
+		return errors.New("must provide name of CA")
+	}
+	formattedReqName := strings.Replace(opts.Host, " ", "_", -1)
+	formattedCAName := strings.Replace(opts.CA, " ", "_", -1)
+
+	if depot.CheckCertificate(d, formattedReqName) {
+		return errors.New("certificate has existed!")
+	}
+
+	csr, err := depot.GetCertificateSigningRequest(d, formattedReqName)
+	if err != nil {
+		return errors.Wrap(err, "problem getting host's certificate signing request")
+	}
+	crt, err := depot.GetCertificate(d, formattedCAName)
+	if err != nil {
+		return errors.Wrap(err, "problem getting CA certificate")
+	}
+
+	// validate that crt is allowed to sign certificates
+	raw_crt, err := crt.GetRawCertificate()
+	if err != nil {
+		return errors.Wrap(err, "problem getting raw CA certificate")
+	}
+	// we punt on checking BasicConstraintsValid and checking MaxPathLen. The goal
+	// is to prevent accidentally creating invalid certificates, not protecting
+	// against malicious input.
+	if !raw_crt.IsCA {
+		return errors.Wrapf(err, "%s is not allowed to sign certificates", opts.CA)
+	}
+
+	var key *pkix.Key
+	if opts.CAPassphrase == "" {
+		key, err = depot.GetPrivateKey(d, formattedCAName)
+		if err != nil {
+			return errors.Wrap(err, "problem getting unencrypted (assumed) CA key")
+		}
+	} else {
+		key, err = depot.GetEncryptedPrivateKey(d, formattedCAName, []byte(opts.CAPassphrase))
+		if err != nil {
+			return errors.Wrap(err, "problem getting encrypted CA key")
+		}
+	}
+
+	expiresTime := time.Now().Add(opts.Expires)
+	var crtOut *pkix.Certificate
+	if opts.Intermediate {
+		crtOut, err = pkix.CreateIntermediateCertificateAuthority(crt, key, csr, expiresTime)
+	} else {
+		crtOut, err = pkix.CreateCertificateHost(crt, key, csr, expiresTime)
+	}
+	if err != nil {
+		return errors.Wrap(err, "problem creating certificate")
+	}
+
+	if err = depot.PutCertificate(d, formattedReqName, crtOut); err != nil {
+		return errors.Wrap(err, "problem saving certificate")
+	}
+
+	return nil
+}
+
+func (opts CertificateOptions) getCertificateRequestName() (string, error) {
+	switch {
+	case opts.CommonName == "":
+		return opts.CommonName, nil
+	case len(opts.Domain) != 0:
+		return opts.Domain[0], nil
+	default:
+		return "", errors.New("must provide a common name or domain!")
+	}
+}
+
+func (opts CertificateOptions) getOrCreatePrivateKey(name string) (*pkix.Key, error) {
+	var key *pkix.Key
+	if opts.Key != "" {
+		keyBytes, err := ioutil.ReadFile(opts.Key)
+		if err != nil {
+			return nil, errors.Wrapf(err, "problem reading key from %s", opts.Key)
+		}
+		key, err = pkix.NewKeyFromPrivateKeyPEM(keyBytes)
+		if err != nil {
+			return nil, errors.Wrapf(err, "problem getting key from PEM")
+		}
+	} else {
+		if opts.KeyBits == 0 {
+			opts.KeyBits = 2048
+		}
+		var err error
+		key, err = pkix.CreateRSAKey(opts.KeyBits)
+		if err != nil {
+			return nil, errors.Wrap(err, "problem creating RSA key")
+		}
+	}
+	return key, nil
+}
+
+func formatName(name string) (string, error) {
+	var filenameAcceptable, err = regexp.Compile("[^a-zA-Z0-9._-]")
+	if err != nil {
+		return "", errors.Wrap(err, "problem compiling regex")
+	}
+	return string(filenameAcceptable.ReplaceAll([]byte(name), []byte("_"))), nil
+}
---
+++ mongo_depot.go
@@ -0,0 +1,237 @@
+package certdepot
+
+import (
+	"time"
+
+	"github.com/mongodb/anser/bsonutil"
+	"github.com/mongodb/grip"
+	"github.com/mongodb/grip/message"
+	"github.com/pkg/errors"
+	"github.com/square/certstrap/depot"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type User struct {
+	ID            string    `bson:"_id"`
+	Cert          string    `bson:"cert"`
+	PrivateKey    string    `bson:"private_key"`
+	CertReq       string    `bson:"cert_req"`
+	CertRevocList string    `bson:"cert_revoc_list"`
+	TTL           time.Time `bson:"ttl"`
+}
+
+var (
+	userIDKey            = bsonutil.MustHaveTag(User{}, "ID")
+	userCertKey          = bsonutil.MustHaveTag(User{}, "Cert")
+	userPrivateKeyKey    = bsonutil.MustHaveTag(User{}, "PrivateKey")
+	userCertReqKey       = bsonutil.MustHaveTag(User{}, "CertReq")
+	userCertRevocListKey = bsonutil.MustHaveTag(User{}, "CertRevocList")
+	userTTLKey           = bsonutil.MustHaveTag(User{}, "TTL")
+)
+
+type mongoCertDepot struct {
+	session        *mgo.Session
+	databaseName   string
+	collectionName string
+	expireAfter    time.Duration
+}
+
+type MgoCertDepotOptions struct {
+	MongoDBURI           string
+	MongoDBDialTimeout   time.Duration
+	MongoDBSocketTimeout time.Duration
+	DatabaseName         string
+	CollectionName       string
+	ExpireAfter          time.Duration
+}
+
+// Create a new cert depot in the specified MongoDB.
+func NewMgoCertDepot(opts MgoCertDepotOptions) (depot.Depot, error) {
+	return newMgoCertDepot(nil, opts)
+}
+
+// Create a new cert depot in the specified MongoDB, using an existing session.
+func NewMgoCertDepotWithSession(s *mgo.Session, opts MgoCertDepotOptions) (depot.Depot, error) {
+	return newMgoCertDepot(s, opts)
+}
+
+func newMgoCertDepot(s *mgo.Session, opts MgoCertDepotOptions) (depot.Depot, error) {
+	if err := opts.validate(); err != nil {
+		return nil, errors.Wrap(err, "invalid options!")
+	}
+
+	if s == nil {
+		var err error
+		s, err = mgo.DialWithTimeout(opts.MongoDBURI, opts.MongoDBDialTimeout)
+		if err != nil {
+			return nil, errors.Wrapf(err, "could not connect to db %s", opts.MongoDBURI)
+		}
+		s.SetSocketTimeout(opts.MongoDBSocketTimeout)
+	}
+
+	return &mongoCertDepot{
+		session:        s,
+		databaseName:   opts.DatabaseName,
+		collectionName: opts.CollectionName,
+		expireAfter:    opts.ExpireAfter,
+	}, nil
+}
+
+func (opts *MgoCertDepotOptions) validate() error {
+	if opts.MongoDBURI == "" {
+		opts.MongoDBURI = "mongodb://localhost:27017"
+	}
+	if opts.MongoDBDialTimeout <= 0 {
+		opts.MongoDBDialTimeout = 2 * time.Second
+	}
+	if opts.MongoDBSocketTimeout <= 0 {
+		opts.MongoDBSocketTimeout = time.Minute
+	}
+	if opts.DatabaseName == "" {
+		opts.DatabaseName = "certDepot"
+	}
+	if opts.CollectionName == "" {
+		opts.CollectionName = "certs"
+	}
+	if opts.ExpireAfter <= 0 {
+		opts.ExpireAfter = 30 * 24 * time.Hour
+	}
+
+	return nil
+}
+
+// Put inserts the data into the document specified by the tag.
+func (m *mongoCertDepot) Put(tag *depot.Tag, data []byte) error {
+	if data == nil {
+		return errors.New("data is nil")
+	}
+
+	name, key := getNameAndKey(tag)
+	session := m.session.Clone()
+	defer session.Close()
+
+	update := bson.M{"$set": bson.M{key: string(data)}}
+	if key == userCertKey {
+		update["$set"].(bson.M)[userTTLKey] = time.Now()
+	}
+	changeInfo, err := session.DB(m.databaseName).C(m.collectionName).UpsertId(name, update)
+	if err != nil {
+		return errors.Wrap(err, "problem adding data to the database")
+	}
+	grip.Debug(message.Fields{
+		"db":     m.databaseName,
+		"coll":   m.collectionName,
+		"id":     name,
+		"change": changeInfo,
+		"op":     "put",
+	})
+
+	return nil
+}
+
+// Check returns whether the user and data specified by the tag exists.
+func (m *mongoCertDepot) Check(tag *depot.Tag) bool {
+	name, key := getNameAndKey(tag)
+	session := m.session.Clone()
+	defer session.Close()
+
+	u := &User{}
+	err := session.DB(m.databaseName).C(m.collectionName).FindId(name).One(u)
+	grip.WarningWhen(errNotNotFound(err), message.Fields{
+		"db":   m.databaseName,
+		"coll": m.collectionName,
+		"id":   name,
+		"err":  err,
+		"op":   "check",
+	})
+
+	switch key {
+	case userCertKey:
+		return u.Cert != ""
+	case userPrivateKeyKey:
+		return u.PrivateKey != ""
+	case userCertReqKey:
+		return u.CertReq != ""
+	case userCertRevocListKey:
+		return u.CertRevocList != ""
+	default:
+		return false
+	}
+}
+
+// Get reads the data for the id specified by tag. Returns an error if
+// the user does not exist, if the TTL has expired (for certs), or if the data
+// is empty.
+func (m *mongoCertDepot) Get(tag *depot.Tag) ([]byte, error) {
+	name, key := getNameAndKey(tag)
+	session := m.session.Clone()
+	defer session.Close()
+
+	u := &User{}
+	err := session.DB(m.databaseName).C(m.collectionName).FindId(name).One(u)
+	if err == mgo.ErrNotFound {
+		return nil, errors.Errorf("could not find %s in the database", name)
+	}
+	if err != nil {
+		return nil, errors.Wrapf(err, "problem looking up %s in the database", name)
+	}
+
+	var data []byte
+	switch key {
+	case userCertKey:
+		data = []byte(u.Cert)
+		if len(data) > 0 && time.Since(u.TTL) > m.expireAfter {
+			return nil, errors.Errorf("certificate for %s has expired!", name)
+		}
+	case userPrivateKeyKey:
+		data = []byte(u.PrivateKey)
+	case userCertReqKey:
+		data = []byte(u.CertReq)
+	case userCertRevocListKey:
+		data = []byte(u.Cert)
+		if len(data) > 0 && time.Since(u.TTL) > m.expireAfter {
+			return nil, errors.Errorf("certificate revocation list for %s has expired!", name)
+		}
+	}
+
+	if len(data) == 0 {
+		return nil, errors.New("no data available!")
+	}
+	return data, nil
+}
+
+// Delete removes the data specified by the tag from a user.
+func (m *mongoCertDepot) Delete(tag *depot.Tag) error {
+	name, key := getNameAndKey(tag)
+	session := m.session.Clone()
+	defer session.Close()
+
+	update := bson.M{"$unset": bson.M{key: ""}}
+	err := m.session.DB(m.databaseName).C(m.collectionName).UpdateId(name, update)
+	if errNotNotFound(err) {
+		return errors.Wrapf(err, "problem deleting %s.%s from the database", name, key)
+	}
+
+	return nil
+}
+
+func errNotNotFound(err error) bool {
+	return err != nil && err != mgo.ErrNotFound
+}
+
+func getNameAndKey(tag *depot.Tag) (string, string) {
+	if name := depot.GetNameFromCrtTag(tag); name != "" {
+		return name, userCertKey
+	}
+	if name := depot.GetNameFromPrivKeyTag(tag); name != "" {
+		return name, userPrivateKeyKey
+	}
+	if name := depot.GetNameFromCsrTag(tag); name != "" {
+		return name, userCertReqKey
+	}
+	if name := depot.GetNameFromCrlTag(tag); name != "" {
+		return name, userCertRevocListKey
+	}
+	return "", ""
+}
